ecc928f6dbf767c06c180457e48c2e94a8db2845
wuhanlin <841116184@qq.com>
2025-10-30 17:38:07 +0100

    Clean up the files

--- ChatApp_BackEnd/src/main/java/com/devStudy/chat/service/interfaces/EmailServiceInt.java
+++
@@ -1,8 +0,0 @@
-package com.devStudy.chat.service.interfaces;
-
-public interface EmailServiceInt {
-
-    // envoyer un message simple à l'adresse email spécifiée
-    void sendSimpleMessage(String to, String subject, String text);
-
-}
